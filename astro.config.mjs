6d346e7a0799ef548c99fa5e43a59454b12ffcfc
Blakeinstein <rishi1998@gmail.com>
2022-03-29 00:30:05 +0530

    bump deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,25 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
-
 // @type-check enabled!
 // VSCode and other TypeScript-enabled text editors will provide auto-completion,
 // helpful tooltips, and warnings if your exported object is invalid.
 // You can disable this by removing "@ts-check" and `@type` comments below.
-
 // @ts-check
-export default /** @type {import('astro').AstroUserConfig} */ ({
-	vite: {
+
+// https://astro.build/config
+export default defineConfig(
+/** @type {import('astro').AstroUserConfig} */
+{
+  vite: {
     ssr: {
-      external: ["svgo"],
-    },
+      external: ["svgo"]
+    }
   },
+  integrations: [tailwind({
+    config: {
+      applyAstroPreset: false
+    }
+  })]
 });
